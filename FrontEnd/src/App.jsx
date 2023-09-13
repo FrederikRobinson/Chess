ed32446d95f56bc7d49eeb250063ab1a90e0afb0
Frederik Robinson <frederik.l.robinson@gmail.com>
2023-09-13 13:08:48 +0100

    Initial setup

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Page here</p>
+    </>
+  )
+}
+
+export default App
---
+++ FrontEnd/src/Components/Board.jsx
@@ -0,0 +1,10 @@
+const Board = () => {
+
+
+
+    return (
+        <>
+            <p>Board here</p>
+        </>
+    )
+}
