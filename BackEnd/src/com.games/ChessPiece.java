3e49910bf8629a3de27a19a3e0ab0d6ee94b2a67
Frederik Robinson <frederik.l.robinson@gmail.com>
2023-09-20 14:35:48 +0100

    Initialised BackEnd

---
+++ BackEnd/src/com.games/ChessPiece.java
@@ -0,0 +1,10 @@
+package com.games;
+
+public class ChessPiece {
+    private final char PIECE_TYPE;
+    private final char PLAYER_COLOUR;
+    public ChessPiece(char pieceType,char playerColour){
+        PIECE_TYPE = pieceType;
+        PLAYER_COLOUR = playerColour;
+    }
+}
