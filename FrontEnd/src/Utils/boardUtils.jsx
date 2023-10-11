df9f8c00c2974f6b9f439517f34748c9181458ff
Frederik Robinson <frederik.l.robinson@gmail.com>
2023-10-11 17:29:56 +0100

    Passing piece onto tiles from board

--- FrontEnd/src/Utils/boardUtils.jsx
+++ FrontEnd/src/Utils/boardUtils.jsx
@@ -1,25 +1,25 @@
 import Tile from '../Components/Tile.jsx'
 
-export const createBoard = () => {
+export const createBoard = (board) => {
     let tiles = [];
     for (let y = 1; y <= 8; y++) {
-        tiles = [...tiles, createRow(y)]
+        tiles = [...tiles, createRow(y, board)]
     }
     return tiles;
 }
 
-const createRow = (y) => {
+const createRow = (y, board) => {
     let row = [];
     for (let x = 1; x <= 8; x++) {
-        row = [...row, createTile(x, y)]
+        row = [...row, createTile(x, y, board[x - 1][y - 1])]
     }
     return row;
 }
 
-const createTile = (x, y) => {
+const createTile = (x, y, piece) => {
     const yPos = y;
     const xPos = posToLetter(x)
-    return <Tile xPos={xPos} yPos={yPos} key={`${xPos} ${yPos}`} />;
+    return <Tile xPos={xPos} yPos={yPos} piece={piece} key={`${xPos} ${yPos}`} />;
 }
 
 export const getTileColour = (xPos, yPos) => {
@@ -39,3 +39,14 @@ const posToLetter = (pos) => {
 const letterToPos = (letter) => {
     return POS_TO_LETTER.indexOf(letter) + 1;
 }
+
+export const emptyBoard = [
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']]
+];
