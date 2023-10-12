fa3198c04f7872ec1f330f886be14f39dbccae11
Frederik Robinson <frederik.l.robinson@gmail.com>
2023-10-12 17:46:51 +0100

    Implemented API call and added basic state

--- FrontEnd/src/Components/Tile.jsx
+++ FrontEnd/src/Components/Tile.jsx
@@ -1,24 +1,24 @@
 import { getTileColour } from "../Utils/boardUtils.jsx";
 import PropTypes from "prop-types";
 
-const Tile = ({ xPos, yPos, piece }) => {
+const Tile = ({ xPos, yPos, piece, functions }) => {
     const tileColour = getTileColour(xPos, yPos);
 
     return (
         <>
-            <div className={`${tileColour} x${xPos} y${yPos} tileContainer`}>
-                <div className={"tileContent"}>{xPos}{yPos}{piece[0]}{piece[1]}</div>
+            <div className={`${tileColour} x${xPos} y${yPos} tileContainer`} onClick={() => functions.handleTileSelection(xPos, yPos)}>
+                <div className={"tileContent"} >{xPos}{yPos}{piece[0]}{piece[1]}</div>
             </div>
         </>
     )
 }
 
-Tile.propTypes = {
-    xPos: PropTypes.string.isRequired,
-    yPos: PropTypes.number.isRequired,
-    piece: PropTypes.arrayOf(
-        PropTypes.string
-    ).isRequired
-}
+// Tile.propTypes = {
+//     xPos: PropTypes.string.isRequired,
+//     yPos: PropTypes.number.isRequired,
+//     piece: PropTypes.arrayOf(
+//         PropTypes.string
+//     ).isRequired
+// }
 
 export default Tile;
--- FrontEnd/src/Utils/boardUtils.jsx
+++ FrontEnd/src/Utils/boardUtils.jsx
@@ -1,25 +1,25 @@
 import Tile from '../Components/Tile.jsx'
 
-export const createBoard = (board) => {
+export const createBoard = (board, functions) => {
     let tiles = [];
     for (let y = 1; y <= 8; y++) {
-        tiles = [...tiles, createRow(y, board)]
+        tiles = [...tiles, createRow(y, board, functions)]
     }
     return tiles;
 }
 
-const createRow = (y, board) => {
+const createRow = (y, board, functions) => {
     let row = [];
     for (let x = 1; x <= 8; x++) {
-        row = [...row, createTile(x, y, board[x - 1][y - 1])]
+        row = [...row, createTile(x, y, board[x - 1][y - 1], functions)]
     }
     return row;
 }
 
-const createTile = (x, y, piece) => {
+const createTile = (x, y, piece, functions) => {
     const yPos = y;
     const xPos = posToLetter(x)
-    return <Tile xPos={xPos} yPos={yPos} piece={piece} key={`${xPos} ${yPos}`} />;
+    return <Tile xPos={xPos} yPos={yPos} piece={piece} functions={functions} key={`${xPos} ${yPos}`} />;
 }
 
 export const getTileColour = (xPos, yPos) => {
@@ -50,3 +50,20 @@ export const emptyBoard = [
     [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
     [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']]
 ];
+export const notEmptyBoard = [
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['P', 'B'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']],
+    [['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X'], ['X', 'X']]
+];
+
+export const nextPlayer = (player) => {
+    if (player == 'W') {
+        return 'B';
+    }
+    return 'W';
+};
