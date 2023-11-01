64d7fe518ed43ded03fbd4d4e604cb1c59f22060
Frederik Robinson <frederik.l.robinson@gmail.com>
2023-11-01 20:28:42 +0000

    Access to database made

--- BackEnd/src/main/java/restservice/RestServiceApplication.java
+++ BackEnd/src/main/java/restservice/RestServiceApplication.java
@@ -3,10 +3,20 @@ package restservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 @SpringBootApplication
 public class RestServiceApplication {
-
+	public static Connection connection;
 	public static void main(String[] args) {
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "student", "student");
+		}
+		catch (Exception e){
+			System.out.println("Failed to connect");
+			System.out.println(e);
+		}
 		SpringApplication.run(RestServiceApplication.class, args);
 	}
 
