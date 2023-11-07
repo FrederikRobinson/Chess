1d3642e5e720af5881eac1e02c1c8a69f557db40
Frederik Robinson <frederik.l.robinson@gmail.com>
2023-11-07 14:15:26 +0000

    Added sign up and login

---
+++ FrontEnd/src/Components/NavBar.jsx
@@ -0,0 +1,9 @@
+const NavBar = ({ userId }) => {
+    return (
+        <>
+            <div>NavBar</div>
+            <div>{userId}</div>
+        </>
+    )
+}
+export default NavBar
--- FrontEnd/src/Utils/apiCalls.js
+++ FrontEnd/src/Utils/apiCalls.js
@@ -10,6 +10,26 @@ export const makeMove = async (startXPos, startYPos, endXPos, endYPos, playerCol
     }
     // return notEmptyBoard;
 }
+export const signUp = async (username, password, email) => {
+    try {
+        const res = await axios.post("http://localhost:8080/signUp", { username, password, email });//, { startXPos, startYPos, endXPos, endYPos, playerColour, gameId });
+        return res.data;
+    }
+    catch (e) {
+        return { error: e.response.data }
+    }
+    // return notEmptyBoard;
+}
+export const login = async (username, password) => {
+    try {
+        const res = await axios.post("http://localhost:8080/login", { username, password });//, { startXPos, startYPos, endXPos, endYPos, playerColour, gameId });
+        return res.data;
+    }
+    catch (e) {
+        return { error: e.response.data }
+    }
+    // return notEmptyBoard;
+}
 export const createGame = async () => {
     try {
         const res = await axios.get("http://localhost:8080/makeMove");//, { startXPos, startYPos, endXPos, endYPos, playerColour, gameId });
