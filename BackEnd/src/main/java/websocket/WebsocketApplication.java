622daef5e7965c28e9d5682b821cdd8c9db0d732
Frederik Robinson <frederik.l.robinson@gmail.com>
2023-11-23 09:42:16 +0000

    More attempts towards websockets

--- BackEnd/src/main/java/websocket/WebsocketApplication.java
+++ BackEnd/src/main/java/websocket/WebsocketApplication.java
@@ -3,10 +3,23 @@ package websocket;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 @SpringBootApplication
 
 public class WebsocketApplication {
+    public static Connection connection;
+
     public static void main(String[] args) {
+
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "student", "student");
+            System.out.println("Successful connection to db");	}
+        catch (Exception e){
+            System.out.println("Failed to connect");
+            System.out.println(e);
+        }
         SpringApplication.run(WebsocketApplication.class, args);
     }
 }
